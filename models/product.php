b6a79190d0a6f4838eb7a55d716cf48ab4911f77
Dungtt19 <19211tt0992@mail.tdc.edu.vn>
2021-11-09 16:38:18 +0700

    first commit1

---
+++ models/product.php
@@ -0,0 +1,34 @@
+<?php
+    class Product extends Db
+    {
+        public function getAllProducts()
+        {
+            $sql = self::$connection->prepare("SELECT * FROM sanpham");
+            $sql->execute(); //return an object
+            $items = array();
+            $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+            return $items; //return an array
+        }
+        public function getProductById($id)
+        {
+            $sql = self::$connection->prepare("SELECT * FROM sanpham WHERE id = ?");
+            $sql->bind_param("i",$id);
+
+            $sql->execute(); //return an object
+            $items = array();
+            $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+            return $items; //return an array
+        }
+        public function search($keyword)
+        {
+            $sql = self::$connection->prepare("SELECT * FROM sanpham WHERE `ten` LIKE ?");
+            $keyword = "%$keyword%";
+            $sql->bind_param("s",$keyword);
+
+            $sql->execute(); //return an object
+            $items = array();
+            $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+            return $items; //return an array
+        }
+    }
+?>
